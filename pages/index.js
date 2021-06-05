c8c9b1677355f77ccf2722829367f5cffa5acf53
ryanda9910 <ryandaaldo@gmail.com>
2021-06-05 10:06:45 +0700

    fix(auth):fixing auth

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,16 @@
 import React,{useEffect} from "react";
 import Router from "next/router";
-import { useAuthState } from "react-firebase-hooks/auth";
-import { auth } from '../module/firebase';
+
 
 export default function Index() {
-  const [user] = useAuthState(auth);
   useEffect(() => {
-    if (user) {
+    if (localStorage.getItem('userId')===null) {
       Router.push("/admin/login");
     } else {
       Router.push("/admin/user");
     }
-  }, [user]);
-  return <div/>
+  }, []);
+  return null
 }
 
 
